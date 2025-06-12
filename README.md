cd0ed1f0ad240316990b1d7d152fcb6168eab28a
ziggysalsa <zelenski.sasha@gmail.com>
2025-06-12 11:03:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Right now, I'm NOT using the settings.json and tasks.json, along with the "trigger on save" extention. 
+
+I'm using watch.sh, using pandoc to convert from MD to HTML, and the weasyprint from HTML to PDF. I can do the weasyprint command in the terminal, but for some reason it's failing as part of watch.sh.
+Failing both with and without the venv
---
+++ watch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INPUT_MD="Zelenski_Resume.md"
+INPUT_CSS="style.css"
+OUTPUT_HTML="${INPUT_MD%.md}.html"
+OUTPUT_PDF="${INPUT_MD%.md}.pdf"
+
+fswatch -o "$INPUT_MD" | while read; do
+  pandoc "$INPUT_MD" -o "$OUTPUT_HTML" --template=resume-template.html --css="$INPUT_CSS" --standalone
+  weasyprint "$OUTPUT_HTML" "$OUTPUT_PDF"
+  echo "Updated $OUTPUT_HTML and $OUTPUT_PDF"
+done
